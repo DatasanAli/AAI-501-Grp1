3218302db902f8fb4b4950394cbf420630909ee3
donsmash29 <67129168+donsmash29@users.noreply.github.com>
2024-12-06 18:42:11 -0700

    EDA file

--- notebooks/util/model_eval.py
+++ notebooks/util/model_eval.py
@@ -2,8 +2,9 @@ from sklearn.metrics import classification_report, roc_auc_score
 
 def predict_and_score(model, features_test, targets_test):
     pred = model.predict(features_test)
-
+    clf_report = classification_report(targets_test, pred)
+    roc_auc = roc_auc_score(targets_test, model.predict_proba(features_test)[:, 1])
     print("\nClassification Report:")
-    print(classification_report(targets_test, pred))
-
-    print('ROC-AUC Score: {}'.format(roc_auc_score(targets_test, model.predict_proba(features_test)[:, 1])))
+    print(clf_report)
+    print('ROC-AUC Score: {}'.format(roc_auc))
+    return clf_report, roc_auc, pred 
