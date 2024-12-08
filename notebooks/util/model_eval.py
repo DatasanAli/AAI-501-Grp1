c6b82dd42619c6e2403813e90efc6ef2d99645e7
omarsagoo <om.sagoo@gmail.com>
2024-12-08 12:56:54 -0700

    add comparisons and update helper function

--- notebooks/util/model_eval.py
+++ notebooks/util/model_eval.py
@@ -1,9 +1,13 @@
-from sklearn.metrics import classification_report, roc_auc_score
+from sklearn.metrics import classification_report, roc_auc_score, precision_recall_fscore_support
 
 def predict_and_score(model, features_test, targets_test):
     pred = model.predict(features_test)
 
+    report = classification_report(targets_test, pred)
     print("\nClassification Report:")
-    print(classification_report(targets_test, pred))
+    print(report)
 
-    print('ROC-AUC Score: {}'.format(roc_auc_score(targets_test, model.predict_proba(features_test)[:, 1])))
+    roc_auc_score_val = roc_auc_score(targets_test, model.predict_proba(features_test)[:, 1])
+    print('ROC-AUC Score: {}'.format(roc_auc_score_val))
+
+    return precision_recall_fscore_support(targets_test, pred), roc_auc_score_val
