ae8293e1330189f3849c95fc0de133e4158a9713
omarsagoo <om.sagoo@gmail.com>
2024-12-09 15:27:18 -0700

    revert issues from redundant work

--- notebooks/util/model_eval.py
+++ notebooks/util/model_eval.py
@@ -1,13 +1,10 @@
-from sklearn.metrics import classification_report, roc_auc_score, precision_recall_fscore_support
+from sklearn.metrics import classification_report, roc_auc_score
 
 def predict_and_score(model, features_test, targets_test):
     pred = model.predict(features_test)
-
-    report = classification_report(targets_test, pred)
+    clf_report = classification_report(targets_test, pred, output_dict=True)
+    roc_auc = roc_auc_score(targets_test, model.predict_proba(features_test)[:, 1])
     print("\nClassification Report:")
-    print(report)
-
-    roc_auc_score_val = roc_auc_score(targets_test, model.predict_proba(features_test)[:, 1])
-    print('ROC-AUC Score: {}'.format(roc_auc_score_val))
-
-    return precision_recall_fscore_support(targets_test, pred), roc_auc_score_val
+    print(clf_report)
+    print("ROC-AUC Score: {}".format(roc_auc))
+    return clf_report, roc_auc
